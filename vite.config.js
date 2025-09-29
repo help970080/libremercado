3ae31ae0cbde05bee92c1dc500fd1eb4828027ab
Leonardo Luna <consultorpremium764@gmail.com>
2025-09-28 21:21:24 -0600

    frontend new

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
+  base: './', // 👈 evita que Render busque mal los assets
 })
